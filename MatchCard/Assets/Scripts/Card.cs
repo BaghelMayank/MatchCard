879c19e5b52d63ef1d547491f09cb8850ffc8649
Mayank <mayank82855@gmail.com>
2025-06-17 00:47:09 +0530

    Add card prefab and Card.cs script with init and flip logic

---
+++ MatchCard/Assets/Scripts/Card.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public Image frontImage;
+    public Image backImage;
+    public Button cardButton;
+
+    [HideInInspector] public int cardID;
+    private bool isFlipped = false;
+
+    public void Init(int id, Sprite frontSprite)
+    {
+        cardID = id;
+        frontImage.sprite = frontSprite;
+        ResetCard();
+    }
+
+    public void Flip()
+    {
+        if(isFlipped) return;
+        isFlipped = true;
+        frontImage.gameObject.SetActive(true);
+        backImage.gameObject.SetActive(false);
+    }
+
+    public void ResetCard()
+    {
+        isFlipped = false;
+        frontImage.gameObject.SetActive(false);
+        backImage.gameObject.SetActive(true);
+    }
+}
