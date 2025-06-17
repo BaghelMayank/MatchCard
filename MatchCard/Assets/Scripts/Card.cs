86c6d637a9ec21b38e008a8d7b9d3c8c9f6e3af1
Mayank <mayank82855@gmail.com>
2025-06-17 12:26:57 +0530

    Flip Card and Matching logic implemented

--- MatchCard/Assets/Scripts/Card.cs
+++ MatchCard/Assets/Scripts/Card.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UI.ProceduralImage;
 
 public class Card : MonoBehaviour
 {
--- MatchCard/Assets/Scripts/GameManager.cs
+++ MatchCard/Assets/Scripts/GameManager.cs
@@ -7,6 +7,10 @@ public class GameManager : MonoBehaviour
     public GameObject cardPrefab;
     public Transform cardsParent;
     public Sprite[] cardFaces;
+
+    private Card firstFlipped = null;
+    private Card secondFlipped = null;
+    private bool isChecking = false;
     
     [SerializeField] private Vector2Int gridSize = new Vector2Int(2,2);
     private List<Card> spawnedCards = new List<Card>();
@@ -34,14 +38,16 @@ public class GameManager : MonoBehaviour
         }
         
         Shuffle(pairIDs);
-
+       
         for (int i = 0; i < totalCards; i++)
         {
             GameObject obj = Instantiate(cardPrefab, cardsParent);
             Card card = obj.GetComponent<Card>();
             card.Init(pairIDs[i],cardFaces[pairIDs[i]]);
+            card.cardButton.onClick.AddListener(() => OnCardClick(card));
             spawnedCards.Add(card);
         }
+        StartCoroutine(PreviewCards());
     }
 
     void Shuffle(List<int> list)
@@ -55,7 +61,58 @@ public class GameManager : MonoBehaviour
 
     void OnCardClick(Card clickedCard)
     {
+        if(isChecking || clickedCard == firstFlipped || clickedCard == secondFlipped)
+            return;
         clickedCard.Flip();
+
+        if (firstFlipped == null)
+        {
+            firstFlipped = clickedCard;
+        }
+        else
+        {
+            secondFlipped = clickedCard;
+            StartCoroutine(CheckMatch());
+        }
     }
-    
+
+    IEnumerator CheckMatch()
+    {
+        isChecking = true;
+        yield return new WaitForSeconds(1f);
+
+        if (firstFlipped.cardID == secondFlipped.cardID)
+        {
+            firstFlipped.cardButton.interactable = false;
+            secondFlipped.cardButton.interactable = false; 
+        }
+        else
+        {
+            firstFlipped.ResetCard();
+            secondFlipped.ResetCard();
+        }
+        
+        firstFlipped = null;
+        secondFlipped = null;
+        isChecking = false;
+    }
+    IEnumerator PreviewCards()
+    {
+        // Flip all cards face up
+        foreach (var card in spawnedCards)
+        {
+            card.Flip();
+            card.cardButton.interactable = false; 
+        }
+
+        yield return new WaitForSeconds(1.5f); 
+
+        // Flip all cards face down
+        foreach (var card in spawnedCards)
+        {
+            card.ResetCard();
+            card.cardButton.interactable = true;
+        }
+    }
+
 }
